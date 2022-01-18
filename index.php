28295507935647aacd2a50b15371868f5f174ceb
Mohammad Jakir Hossain <jakir_comilla1@yahoo.com>
2022-01-18 16:55:12 +0600

    Index File Uploaded

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</heaD>
+<body>
+<?php
+$time = date("H");
+if  ($time < "13"){
+	echo "have a good day";
+} else {
+	echo "Have a good night";
+}
+
+
+?>
+
+
+
+</body>
+</html>
